273b0118d3e4f82274ada4da33cf16df94b21e5f
Aditya Chivukula <aditya.chivukula@gmail.com>
2018-07-14 12:17:46 +0200

    updated CipherMatrix, matmul not working yet

--- ahem/ciphermatrix.py
+++ ahem/ciphermatrix.py
@@ -31,13 +31,14 @@ class CipherMatrix:
         """
 
         self.parms = EncryptionParameters()
-        self.parms.set_poly_modulus("1x^2048 + 1")
-        self.parms.set_coeff_modulus(seal.coeff_modulus_128(2048))
+        self.parms.set_poly_modulus("1x^8192 + 1")
+        self.parms.set_coeff_modulus(seal.coeff_modulus_128(8192))
         self.parms.set_plain_modulus(1 << 8)
 
         self.context = SEALContext(self.parms)
 
         self.encoder = IntegerEncoder(self.context.plain_modulus())
+        self.floatencoder = FractionalEncoder(self.context.plain_modulus(), self.context.poly_modulus(), 64, 32, 3)
 
         self.keygen = KeyGenerator(self.context)
         self.public_key = self.keygen.public_key()
@@ -46,7 +47,9 @@ class CipherMatrix:
         self.encryptor = Encryptor(self.context, self.public_key)
         self.decryptor = Decryptor(self.context, self.secret_key)
 
+        self.evaluator = Evaluator(self.context)
 
+        self._encrypted = False
 
         if matrix is not None:
             assert len(matrix.shape) == 2, "Only 2D numpy matrices accepted currently"
@@ -69,6 +72,83 @@ class CipherMatrix:
 
         else:
             return '[]'
+
+    def __str__(self):
+        """
+
+        :return:
+        """
+        print("| Encryption parameters:")
+        print("| poly_modulus: " + self.context.poly_modulus().to_string())
+
+        # Print the size of the true (product) coefficient modulus
+        print("| coeff_modulus_size: " + (str)(self.context.total_coeff_modulus().significant_bit_count()) + " bits")
+
+        print("| plain_modulus: " + (str)(self.context.plain_modulus().value()))
+        print("| noise_standard_deviation: " + (str)(self.context.noise_standard_deviation()))
+
+        if self.matrix is not None:
+            print(self.matrix.shape)
+
+        return str(type(self))
+
+    def __mul__(self, other):
+        """
+
+        :param other:
+        :return:
+        """
+
+        assert isinstance(other, CipherMatrix), "Can only be multiplied with a cipher matrix"
+
+        result = CipherMatrix()
+
+        A = self.encrypted_matrix
+        B = other.encrypted_matrix
+
+        # A = self.matrix
+        # B = other.matrix
+
+        Ashape = A.shape
+        Bshape = B.shape
+
+        assert Ashape[1] == Bshape[0], "Dimensionality mismatch"
+        result_shape = [Ashape[0], Bshape[1]]
+
+        result = CipherMatrix(np.zeros(result_shape, dtype=np.int32))
+        plain_result = np.zeros(result_shape, dtype=np.int32)
+
+        for i in range(Ashape[0]):
+            for j in range(Bshape[1]):
+                val = Ciphertext(self.parms)
+                self.encryptor.encrypt(self.encoder.encode(0), val)
+
+                result_array = []
+                for k in range(Ashape[1]):
+                    res = Ciphertext(self.parms)
+                    result_array.append(res)
+                    print("Noise budget in val: " + (str)(
+                        self.decryptor.invariant_noise_budget(res)) + " bits")
+                    self.evaluator.multiply(A[i, k], B[k, j], result_array[k])
+                    print(i, j, k)
+
+
+                    # self.evaluator.add(val, res)
+                    del res
+
+                    #
+                    #
+                    # temp = A[i,k] * B[k,j]
+                    # plain_result[i,j] = plain_result[i,j] + temp
+                    #
+
+
+                self.evaluator.add_many(result_array, val)
+                self.evaluator.add(result.encrypted_matrix[i,j], val)
+
+
+        return result
+        # return plain_result
 
     def save(self, path):
         """
@@ -94,7 +174,7 @@ class CipherMatrix:
         """
 
         if matrix is not None:
-            assert self.matrix is not None, "matrix already exists"
+            assert self.matrix is None, "matrix already exists"
             self.matrix = np.copy(matrix)
 
         shape = self.matrix.shape
@@ -107,13 +187,15 @@ class CipherMatrix:
                 self.encrypted_matrix[i,j] = Ciphertext()
                 self.encryptor.encrypt(val, self.encrypted_matrix[i,j])
 
+        self._encrypted = True
+
     def decrypt(self):
         """
 
         :return:
         """
 
-        assert self.encrypted_matrix is not None, "No encrypted matrix"
+        assert self._encrypted and self.encrypted_matrix is not None, "No encrypted matrix"
         del self.matrix
         shape = self.encrypted_matrix.shape
 
@@ -125,7 +207,8 @@ class CipherMatrix:
                 self.decryptor.decrypt(self.encrypted_matrix[i,j], plain_text)
                 self.matrix[i,j] = self.encoder.decode_int32(plain_text)
 
-        return self.matrix
+        self._encrypted = False
+        return np.copy(self.matrix)
 
 def test():
     print('blah')
--- test-seal.py
+++ test-seal.py
@@ -1,3 +1,21 @@
-import seal
+import ahem
+import numpy as np
 
-print('yay')
+ahem.test()
+
+mat = np.ones([2, 2], dtype=np.int32)
+cm = ahem.CipherMatrix(mat)
+b = ahem.CipherMatrix(mat)
+
+res = b * cm
+print(res)
+print(res.decrypt())
+#
+# cm.encrypt(mat)
+
+# print(cm)
+#
+# new_mat = cm.decrypt()
+#
+# print(new_mat)
+# print(type(new_mat))

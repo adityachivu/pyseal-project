119704a7cca6fab657cf2a33ab552fa66c914332
Aditya Chivukula <aditya.chivukula@gmail.com>
2018-07-15 15:15:18 +0200

    included cloud evals in main namespace

--- ahem/__init__.py
+++ ahem/__init__.py
@@ -1,6 +1,9 @@
 from . import ciphermatrix
 from .ciphermatrix import *
 
+from . import cloud
+from .cloud import evaluate
+
 parms = EncryptionParameters()
 parms.set_poly_modulus("1x^8192 + 1")
 parms.set_coeff_modulus(seal.coeff_modulus_128(8192))

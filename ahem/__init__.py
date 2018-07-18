7bada27260d0aa87b07789b5b2f071f9217ca571
Aditya Chivukula <aditya.chivukula@gmail.com>
2018-07-18 13:56:55 +0200

    modified namespace

--- ahem/__init__.py
+++ ahem/__init__.py
@@ -7,8 +7,8 @@ from .cloud import *
 from .cloud.evaluate import *
 
 parms = EncryptionParameters()
-parms.set_poly_modulus("1x^8192 + 1")
-parms.set_coeff_modulus(seal.coeff_modulus_128(8192))
+parms.set_poly_modulus("1x^2048 + 1")
+parms.set_coeff_modulus(seal.coeff_modulus_128(2048))
 parms.set_plain_modulus(1 << 8)
 
 context = SEALContext(parms)

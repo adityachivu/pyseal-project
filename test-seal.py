f95da7bf3f4274cf426f9aba882c9765f72bbce6
Aditya Chivukula <aditya.chivukula@gmail.com>
2018-07-18 22:46:06 +0200

    cleaned up, added all functionality

--- test-seal.py
+++ test-seal.py
@@ -1,5 +1,6 @@
 import ahem
 import numpy as np
+import time
 
 ahem.test()
 port = int(input("Enter port:"))
@@ -7,8 +8,8 @@ ahem.setup_host("172.17.0.3", port)
 
 keygen = ahem.KeyGenerator(ahem.context)
 
-# mat = np.random.randint(0, 10, [3,8])
-mat = np.random.random(size=[4,4])
+length = int(input("Enter matrix side length: "))
+mat = np.random.random(size=[length,length])
 mat2 = 2*mat
 mat2 = mat2.T
 
@@ -16,6 +17,7 @@ print("\nMatrix 1:")
 print(mat)
 print("\nMatrix 2:")
 print(mat2)
+time.sleep(5)
 
 print("\nCipherMatrix A: ", end='')
 A = ahem.CipherMatrix()
@@ -25,23 +27,27 @@ B = ahem.CipherMatrix()
 A.encrypt(mat, keygen)
 B.encrypt(mat2, keygen)
 
+print("\n\n\n--------------------ADDITION---------------------")
 result = ahem.cloud.add_request(A, B)
 print("\nResult Loaded, Decrypted")
 res_mat = result.decrypt(keygen=keygen)
 print(res_mat)
 print("\nExpected Result:")
-print(np.matmul(mat, mat2))
-
-result = ahem.cloud.subtract_request(A, B)
+print(mat + mat2)
+time.sleep(5)
+print("\n\n\n-------------------SUBTRACTION-------------------")
+result = ahem.cloud.sub_request(A, B)
 print("\nResult Loaded, Decrypted")
 res_mat = result.decrypt(keygen=keygen)
 print(res_mat)
 print("\nExpected Result:")
-print(np.matmul(mat, mat2))
-
+print(mat-mat2)
+time.sleep(5)
+print("\n\n\n------------------MULTIPLICATION-----------------")
 result = ahem.cloud.multiply_request(A, B)
 print("\nResult Loaded, Decrypted")
 res_mat = result.decrypt(keygen=keygen)
 print(res_mat)
 print("\nExpected Result:")
 print(np.matmul(mat, mat2))
+

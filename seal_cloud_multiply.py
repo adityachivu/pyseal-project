b30088c82045969d9f8ee4b6cc47499c43fada9c
Aditya Chivukula <aditya.chivukula@gmail.com>
2018-07-18 13:56:14 +0200

    updated output for and exit condition

--- seal_cloud_multiply.py
+++ seal_cloud_multiply.py
@@ -2,43 +2,22 @@ import ahem
 import os
 
 
-def process_multiply_request():
+def process_multiply_request(serversock):
     """
 
+    :param serversock:
     :return:
     """
-    clientsocket, addr = serversock.accept()
-    print("Got a connection from %s" % str(addr))
-    result = recv_CipherMatrix(clientsocket, "/seal-project/receive/")
-    print(result)
-
-    clientsocket, addr = serversock.accept()
-    print("Got a connection from %s" % str(addr))
-    result = recv_CipherMatrix(clientsocket, "/seal-project/receive2/")
-    print(result)
-
-    clientsocket, addr = serversock.accept()
-    print("Got a connection from %s" % str(addr))
-    send_CipherMatrix(clientsocket, "/seal-project/result/")
-
-
-
-
-def main():
-    print("SEAL MULTIPLY SERVER")
-    ahem.PORT = int(input("Enter post number:"))
-
-    serversock = ahem.setup_server(ahem.PORT)
 
     root_path = "/seal-project/server/"
-
     A_path = "/seal-project/server/A/"
+    B_path = "/seal-project/server/B/"
+
     clientsocket, addr = serversock.accept()
     print("Got a connection from %s" % str(addr))
     ahem.recv_CipherMatrix(clientsocket, A_path)
     # print(result)
 
-    B_path = "/seal-project/server/B/"
     clientsocket, addr = serversock.accept()
     print("Got a connection from %s" % str(addr))
     ahem.recv_CipherMatrix(clientsocket, B_path)
@@ -50,21 +29,34 @@ def main():
     B = ahem.CipherMatrix()
     B.load(B_path)
 
-
     res = A * B
 
     for file in os.listdir(A_path):
-        os.remove(os.path.join(A_path,file))
+        os.remove(os.path.join(A_path, file))
     for file in os.listdir(B_path):
-        os.remove(os.path.join(B_path,file))
+        os.remove(os.path.join(B_path, file))
 
-    result_path = res.save("/seal-project/server/")
+    result_path = res.save(root_path)
 
     clientsocket, addr = serversock.accept()
     print("Got a connection from %s" % str(addr))
     ahem.send_CipherMatrix(clientsocket, result_path)
 
-    input("Press Enter to exit")
+    close_server = int(input("Press Enter to exit"))
+
+
+
+
+def main():
+
+    print("SEAL MULTIPLY SERVER")
+    ahem.PORT = int(input("Enter port:"))
+
+    serversock = ahem.setup_server(ahem.PORT)
+
+    close_server = True
+    while(close_server):
+        process_multiply_request(serversock)
 
     serversock.close()
 
